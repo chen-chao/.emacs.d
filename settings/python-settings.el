51ff8efd00ca862bf3814fb002a36af44688b24b
chen-chao <wenbushi@gmail.com>
2017-06-14 01:57:37 +0000

    py-autopep8 and ipython

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -1,3 +1,8 @@
+;; auto pair
+(electric-pair-mode 1)
+(setq electric-pair-pairs '((?\" . ?\")
+                            (?\{ . ?\}) ))
+
 (setq python-indent-offset 4)
 ;; python auto completion
 (add-hook 'python-mode-hook 'jedi:setup)
@@ -6,33 +11,16 @@
 ;; python syntax check
 (add-hook 'python-mode-hook 'flycheck-mode)
 
-;; using anaconda mode
-;; (add-hook 'python-mode-hook 'anaconda-mode)
-;; (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-;; (add-hook 'python-mode-hook 'ac-anaconda-setup)
-
-;; auto indent yanked/pasted text
-(dolist (command '(yank yank-pop))
-   (eval `(defadvice ,command (after indent-region activate)
-            (and (not current-prefix-arg)
-                 (member major-mode '(emacs-lisp-mode lisp-mode
-                                                      clojure-mode    scheme-mode
-                                                      haskell-mode    ruby-mode
-                                                      rspec-mode      python-mode
-                                                      c-mode          c++-mode
-                                                      objc-mode       latex-mode
-                                                      plain-tex-mode))
-                 (let ((mark-even-if-inactive transient-mark-mode))
-                   (indent-region (region-beginning) (region-end) nil))))))
-
-;; auto pair
-(electric-pair-mode 1)
-(setq electric-pair-pairs '((?\" . ?\")
-                            (?\{ . ?\}) ))
+;; auto pep8 format
+;; need `pip install autopep8`
+(require 'py-autopep8)
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+(setq py-autopep8-options '("--max-line-length=100"))
 
 ;; python interpreter
 (when (executable-find "ipython")
-   (setq python-shell-interpreter "ipython"))
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "--simple-prompt -i"))
 
 ;; markdown-mode
 ;; from https://github.com/purcell/emacs.d/blob/master/lisp/init-markdown.el
