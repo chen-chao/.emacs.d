f30373866174e41041c985e35db579dd38b713df
chen-chao <wenbushi@gmail.com>
2017-12-10 23:35:32 +0800

    set flycheck-pylintrc

--- settings/init-flycheck.el
+++ settings/init-flycheck.el
@@ -4,16 +4,10 @@
   :init (global-flycheck-mode)
   :config
   (setq flycheck-emacs-lisp-load-path "inherit")
-  (progn
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (setq flycheck-standard-error-navigation nil)
-    ;; flycheck errors on a tooltip (doesn't work on console)
-    ;; (when (display-graphic-p (selected-frame))
-    ;; (eval-after-load 'flycheck '(custom-set-variables
-    ;; '(flycheck-display-errors-function
-    ;; #'flycheck-pos-tip-error-messages))))
-    ;; (add-hook 'python-mode-hook 'flycheck-mode)
-    )
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (setq flycheck-standard-error-navigation nil)
+  (if (file-exists-p "~/.pylint.d/pylintrc")
+      (setq flycheck-pylintrc "~/.pylint.d/pylintrc"))
   )
 
 (provide 'init-flycheck)
