d2ee3b00b286334b4c34f9368b879155b763075b
chen-chao <wenbushi@gmail.com>
2017-12-10 22:45:22 +0800

    automatically create org files if not existed

--- settings/org-settings.el
+++ settings/org-settings.el
@@ -18,14 +18,21 @@
              ;;(add-to-list 'org-modules' "org-habit")
 
              ;; set default target file for notes
+             (unless (file-directory-p "~/org/") (make-directory "~/org/"))
              (setq org-directory "~/org/")
-             (setq org-default-notes-file (concat org-directory "/notes.org"))
-             
+             (setq org-notes-file (concat org-directory "/notes.org"))
+             (unless (file-exists-p org-notes-file) (write-region "" nil org-notes-file))
+             (setq org-default-notes-file org-notes-file)
+
              ;; capture templates
+             (setq org-gtd-file (concat org-directory "/gtd.org"))
+             (unless (file-exists-p org-gtd-file) (write-region "" nil org-gtd-file))
+             (setq org-journal-file (concat org-directory "/journal.org"))
+             (unless (file-exists-p org-journal-file) (write-region "" nil org-journal-file))
              (setq org-capture-templates
-                   '(("t" "TODO" entry (file+headline "~/org/gtd.org" "Tasks")
+                   '(("t" "TODO" entry (file+headline org-gtd-file "Tasks")
                       "* TODO %?\n %i\n %a")
-                     ("j" "Journal" entry (file+datetree "~/org/journal.org")
+                     ("j" "Journal" entry (file+datetree org-journal-file)
                       "* %?\nEntered on %U\n %i\n %a")
                      ))
              (add-to-list 'org-structure-template-alist
