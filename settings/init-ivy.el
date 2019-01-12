87831d746d44f345453aff5c05e3bec424f3396d
chen-chao <wenbushi@gmail.com>
2019-01-12 23:46:32 +0800

    set up ivy

--- settings/init-ivy.el
+++ settings/init-ivy.el
@@ -1,19 +1,19 @@
 ;; init ivy mode
 
-(require 'which-key)
-
-(which-key-mode)
-
 (use-package ivy
   :ensure t
   :diminish (ivy-mode . "") ; does not display ivy in the modeline
   :init (ivy-mode 1)        ; enable ivy globally at startup
-  :bind (:map ivy-mode-map  ; bind in the ivy buffer
-              ("C-'" . ivy-avy)) ; C-' to ivy-avy
+  :bind
+  (:map ivy-mode-map			; bind in the ivy buffer
+        ("C-'"     . ivy-avy)		; C-' to ivy-avy
+	("C-c C-r" . ivy-resume)	; recalls completion session
+	)
   :config
   (setq ivy-use-virtual-buffers t)   ; extend searching to bookmarks and …
   (setq ivy-height 20)               ; set height of the ivy window
   (setq ivy-count-format "(%d/%d) ") ; count format, from the ivy help page
+  (setq ivy-wrap t)		     ; enable "C-n" "C-p" to cycle past the first and last candidates
   )
 
 (use-package counsel
@@ -22,10 +22,22 @@
   (("M-x"     . counsel-M-x)       ; M-x use counsel
    ("C-x C-f" . counsel-find-file) ; C-x C-f use counsel-find-file
    ("C-x C-r" . counsel-recentf)   ; search recently edited files
-   ("C-c f"   . counsel-git)       ; search for files in git repo
+   ("C-c g"   . counsel-git)       ; search for files in git repo
    ("C-c s"   . counsel-git-grep)  ; search for regexp in git repo
-   ("C-c /"   . counsel-ag)        ; search for regexp in git repo using ag
-   ("C-c l"   . counsel-locate))   ; search for files or else using locate
+   ("C-c k"   . counsel-ag)        ; search for regexp in git repo using ag
+   ("C-c l"   . counsel-locate)    ; search for files or else using locate
+   ("C-h f"   . counsel-describe-function)
+   ("C-h v"   . counsel-describe-variable)
+   ("C-h i"   . counsel-info-lookup-symbol)
+   ("C-h l"   . counsel-find-library)
+   ("C-h u"   . counsel-unicode-char)
+   )
+  )
+
+(use-package swiper
+  :ensure t
+  :bind
+  (("C-s" . swiper))
   )
 
 (provide 'init-ivy)
