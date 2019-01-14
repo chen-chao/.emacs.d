563d5ee3d6d67e6d2cb01eeb62fc36e811265a4d
chen-chao <wenbushi@gmail.com>
2019-01-14 21:20:14 +0800

    org mode settings

--- settings/org-settings.el
+++ settings/org-settings.el
@@ -1,53 +1,62 @@
 ;; org-mode settings
+
+(defun prepare-org-files ()
+  (unless (file-directory-p "~/org/") (make-directory "~/org/"))
+  (unless (file-exists-p "~/org/notes.org") (write-region "" nil "~/org/notes.org"))
+  (unless (file-exists-p "~/org/gtd.org") (write-region "" nil "~/org/gtd.org"))
+  (unless (file-exists-p "~/org/journal.org") (write-region "" nil "~/org/journal.org"))
+  )
+
+
 (use-package org
   :ensure t
 
-  :general
-
-  (general-nmap "SPC c" 'org-capture)
-  (general-nmap "SPC a" 'org-agenda)
-  
-  :bind (("C-c l" . org-store-link)
-         ("C-c a" . org-agenda)
-         ("C-c b" . org-iswitchb)
-         ("C-c c" . org-capture))
+  :bind
+  (("C-c l" . org-store-link)
+   ("C-c a" . org-agenda)
+   ("C-c b" . org-iswitchb)
+   ("C-c c" . org-capture)
+   :map org-mode-map
+   ("C-c z" . org-add-note)
+   ("C-c <" . outline-promote)
+   ("C-c >" . outline-demote)
+   ("C-c [" . org-agenda-file-to-front)
+   ("C-c ]" . org-remove-file)
+   )
 
   :config
-  ;; auto expand item tree
-  (setq org-startup-folded nil)
   (setq org-log-done 'time)
   (setq org-log-done 'note)
-  ;; notes into logbook drawer
-  ;; can also using property for individual todo items with :LOG_INTO_DRAWER: drawername
-  ;; (setq org-log-into-drawer t)
   (setq org-todo-keywords '((sequence "TODO" "|" "DONE" "CANCELED(c!)") ) )
 
-  ;; (require 'org-habit)
-  (add-to-list 'org-modules 'org-habit)
+  (require 'org-habit)
+  ;; (add-to-list 'org-modules 'org-habit)
 
-  ;; set default target file for notes
-  (unless (file-directory-p "~/org/") (make-directory "~/org/"))
-  (setq org-directory "~/org/")
-  (setq org-notes-file (concat org-directory "/notes.org"))
-  (unless (file-exists-p org-notes-file) (write-region "" nil org-notes-file))
-  (setq org-default-notes-file org-notes-file)
+  (prepare-org-files)
 
+  ;; refile targets
+  (setq org-refile-targets '((nil . (:level . 1))
+                             ("~/org/notes.org" . (:level . 1))
+                             ("~/org/gtd.org" . (:level . 1))))
+  
   ;; capture templates
-  (setq org-gtd-file (concat org-directory "/gtd.org"))
-  (unless (file-exists-p org-gtd-file) (write-region "" nil org-gtd-file))
-  (setq org-journal-file (concat org-directory "/journal.org"))
-  (unless (file-exists-p org-journal-file) (write-region "" nil org-journal-file))
   (setq org-capture-templates
-        '(("t" "TODO" entry (file+headline org-gtd-file "Tasks")
-           "* TODO %?\n %i\n %a")
-          ("j" "Journal" entry (file+datetree org-journal-file)
-           "* %?\nEntered on %U\n %i\n")
-          ("n" "Note" item (file org-notes-file)
-           "* %?\nEntered on %U\n %i\n %a")
-          ))
+  '(("t" "TODO" entry (file+headline "~/org/gtd.org" "Tasks") "* TODO %?\n %i\n %a")
+    ("j" "Journal" entry (file+datetree "~/org/journal.org") "* %?\nEntered on %U\n %i\n")
+    ("n" "Note" item (file+headline "~/org/notes.org" "Notes") "%?\nEntered on %U\n %i\n %a")))
+  
   (add-to-list 'org-structure-template-alist
                '("p" "#+BEGIN_SRC python\n?\n#+END_SRC")
                '("l" "#+BEGIN_SRC shell\n?\n#+END_SRC"))
+
+  ;; org html export
+  ;; remove xml header
+  (setq org-html-xml-declaration
+        '(("html" . "")
+          ("xml" . "<?xml version=\"1.0\" encoding=\"%s\"?>")
+          ("php" . "<?php echo \"<?xml version=\\\"1.0\\\" encoding=\\\"%s\\\" ?>\"; ?>")))
+  (setq org-html-preamble nil)
+  (setq org-html-postamble nil)
   )
 
 (provide 'org-settings)
