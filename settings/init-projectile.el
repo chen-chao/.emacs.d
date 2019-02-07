29daef879c3fcf97a5e6e802b507efb10ba0237e
chen-chao <wenbushi@gmail.com>
2019-02-07 13:32:34 +0800

    redefine treemacs key bindings

--- settings/init-projectile.el
+++ settings/init-projectile.el
@@ -11,12 +11,12 @@
   :defer t
   :bind
   (:map global-map
-        ("C-c p"     . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag))
+        ("C-c t p"     . treemacs-select-window)
+        ("C-c t 1"   . treemacs-delete-other-windows)
+        ("C-c t t"   . treemacs)
+        ("C-c t B"   . treemacs-bookmark)
+        ("C-c t C-t" . treemacs-find-file)
+        ("C-c t M-t" . treemacs-find-tag))
   :config
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
@@ -32,13 +32,18 @@
 
 (use-package treemacs-projectile
   :ensure t
-  :defer t
   :after treemacs projectile
   )
 
 (use-package treemacs-icons-dired
+  :ensure t
   :after treemacs dired
   :config (treemacs-icons-dired-mode)
+  )
+
+(use-package treemacs-magit
+  :ensure t
+  :after treemacs magit
   )
 
 
