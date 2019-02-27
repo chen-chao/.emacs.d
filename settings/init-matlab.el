3a6c1cb774018a28614999bf42b4735572d4325f
chen-chao <wenbushi@gmail.com>
2019-02-27 18:05:35 +0800

    settings for matlab .m files

--- settings/init-matlab.el
+++ settings/init-matlab.el
@@ -5,17 +5,10 @@
 
 (defun fix-octave-comments ()
     (make-local-variable 'comment-start)
-    (set 'comment-start "%")
-    (set 'comment-column 0))
+    (setq comment-start "%")
+    (setq comment-column 0))
 
 (add-hook 'octave-mode-hook 'fix-octave-comments)
-
-(add-hook 'octave-mode-hook
-          (lambda ()
-            (abbrev-mode 1)
-            (auto-fill-mode 1)
-            (if (eq window-system 'x)
-                (font-lock-mode 1))))
 
 (provide 'matlab-settings)
 
