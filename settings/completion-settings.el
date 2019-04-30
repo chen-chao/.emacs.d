852cf8fb36c848dff5dfb02d8d39d5b7775107ac
chen-chao <wenbushi@gmail.com>
2019-04-30 23:34:08 +0800

    lsp mode for golang, python and c++

--- settings/completion-settings.el
+++ settings/completion-settings.el
@@ -18,11 +18,25 @@
 (use-package lsp-mode
   :ensure t
   :commands lsp
+  :init (setq lsp-auto-guess-root nil)
+  :hook ((python-mode c++-mode go-mode) . (lambda () (lsp)))
   :config
   (require 'lsp-clients)
-  (add-hook 'python-mode-hook #'lsp)
-  (add-hook 'c++-mode-hook #'lsp)
-  (add-hook 'java-mode-hook #'lsp)
+  (setq lsp-clients-python-library-directories '("/usr/local/" "/usr/"))
+
+  ;; (add-hook 'python-mode-hook #'lsp)
+  ;; (add-hook 'c++-mode-hook #'lsp)
+
+  ;; Pre-install:
+  ;; go get -u golang.org/x/tools/cmd/gopls
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection "gopls")
+		    :major-modes '(go-mode)
+		    :priority 0
+		    :initialization-options 'lsp-clients-go--make-init-options
+		    :server-id 'gopls
+		    :library-folders-fn (lambda (_workspace)
+					  lsp-clients-go-library-directories)))
   )
 
 (use-package lsp-ui
@@ -32,7 +46,17 @@
   (:map lsp-ui-mode-map
 	([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
 	([remap xref-find-references] . lsp-ui-peek-find-references)
-	)
+	("C-c u" . lsp-ui-imenu))
+  :init
+  (setq lsp-ui-doc-enable t
+	lsp-ui-doc-header t
+	lsp-ui-doc-include-signature t
+	lsp-ui-doc-position 'top
+	lsp-ui-doc-use-webkit t
+	lsp-ui-doc-border (face-foreground 'default)
+
+	lsp-ui-sideline-enable nil
+	lsp-ui-sideline-ignore-duplicate t)
   :config
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
   (setq lsp-ui-doc-use-childframe t)
@@ -41,19 +65,28 @@
 
 (use-package company-lsp
   :ensure t
-  :requires company
-  :commands company-lsp
+  :init (setq company-lsp-cache-candidates 'auto)
   :config
   (push 'company-lsp company-backends)
   )
 
+;; C/C++/Objective-C support
+(use-package ccls
+  :ensure t
+  :defines projectile-project-root-files-top-down-recurring
+  :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda ()
+						   (require 'ccls)
+						   (lsp)))
+  :config
+  (with-eval-after-load 'projectile
+    (setq projectile-project-root-files-top-down-recurring
+	  (append '("compile_commands.json"
+		    ".ccls")
+		  projectile-project-root-files-top-down-recurring))))
+
 (use-package lsp-java
   :ensure t
-  :after lsp
-  :config
-  (add-hook 'java-mode-hook #'lsp)
-  ;; (setq lsp-java-server-install-dir "~/.emacs.d/site-lisp/eclipse.jdt.ls/server/")
-  )
+  :hook (java-mode . (lambda () (require 'lsp-java) (lsp))))
 
 (use-package yasnippet
   :ensure t
