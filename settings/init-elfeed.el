c0c20f91c660771592afa09f9a73c02535ffa4f6
chen-chao <wenbushi@gmail.com>
2019-06-29 19:52:36 +0800

    keybindings for elfeed

--- settings/init-elfeed.el
+++ settings/init-elfeed.el
@@ -1,6 +1,9 @@
 (use-package elfeed
+  :commands (elfeed elfeed-update)
+  :bind (:map elfeed-search-mode-map
+	      ("g" . elfeed-update)
+	      )
   :config
-
   ;; set face attribute
   (push 'elfeed-search-title-face zh-align-faces)
   (push 'elfeed-search-feed-face zh-align-faces)
@@ -9,23 +12,5 @@
     :config
     (setq rmh-elfeed-org-files (list "~/.emacs.d/settings/elfeed.org"))
     (elfeed-org))
-
-  ;; automatically update feeds
-  (when (>= emacs-major-version 26)
-
-    (defvar elfeed-update-timer nil)
-
-    (defun elfeed-cancel-timer ()
-      (interactive)
-      (and elfeed-update-timer (cancel-timer elfeed-update-timer))
-      (setq elfeed-update-timer nil))
-
-    (with-eval-after-load 'elfeed
-      ;; (message "load elfeed")
-      (elfeed-cancel-timer)
-      (setq elfeed-update-timer
-	    (run-with-timer 0 3600 (lambda () (make-thread 'elfeed-update))))
-      )
-    )
   )
 (provide 'init-elfeed)
