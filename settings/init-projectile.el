34cd48dceb9c52d48a551b886507e2e7a68d40d3
chenchao <wenbushi@gmail.com>
2020-01-09 11:12:44 +0800

    remove projectile and treemacs

--- settings/init-projectile.el
+++ settings/init-projectile.el
@@ -1,49 +1,10 @@
 (use-package projectile
-  :demand t
+  ;; :demand t
   :delight '(:eval (concat " Proj[" (projectile-project-name) "]"))
-  :init
-  (projectile-mode 1)
+  ;; :init
+  ;; (projectile-mode 1)
   :bind-keymap
   ("C-c p" . projectile-command-map)
   )
-
-(use-package treemacs
-  :defer t
-  :bind
-  (:map global-map
-        ("C-c t p"   . treemacs-select-window)
-        ("C-c t 1"   . treemacs-delete-other-windows)
-        ("C-c t t"   . treemacs)
-        ("C-c t B"   . treemacs-bookmark)
-        ("C-c t C-t" . treemacs-find-file)
-        ("C-c t M-t" . treemacs-find-tag)
-	:map treemacs-mode-map
-	([mouse-1]   . treemacs-RET-action)
-	)
-  :config
-  (treemacs-follow-mode t)
-  (treemacs-filewatch-mode t)
-  (treemacs-fringe-indicator-mode t)
-  (setq treemacs-width 20)
-  (pcase (cons (not (null (executable-find "git")))
-               (not (null (executable-find "python3"))))
-    (`(t . t)
-     (treemacs-git-mode 'deferred))
-    (`(t . _)
-     (treemacs-git-mode 'simple)))
-  )
-
-(use-package treemacs-projectile
-  :after treemacs projectile)
-
-(use-package treemacs-icons-dired
-  :after treemacs dired
-  :config (treemacs-icons-dired-mode)
-  )
-
-(use-package treemacs-magit
-  :after treemacs magit
-  )
-
 
 (provide 'init-projectile)
