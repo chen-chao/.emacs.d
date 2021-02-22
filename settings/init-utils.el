c46a24d237cf18859576742fbdf42f4a74337f0b
chen-chao <wenbushi@gmail.com>
2021-02-23 00:19:00 +0800

    valign elfeed

--- settings/init-utils.el
+++ settings/init-utils.el
@@ -106,38 +106,38 @@
       (setf elfeed-curl-extra-arguments `("--socks5-hostname" ,host))))
 
   (require 'valign)
-  (defun elfeed-search-print-entry (entry)
-  "Print ENTRY to the buffer."
-  (let* ((date (elfeed-search-format-date (elfeed-entry-date entry)))
-	 (date-width (car (cdr elfeed-search-date-format)))
-         (title (or (elfeed-meta entry :title) (elfeed-entry-title entry) ""))
-         (title-faces (elfeed-search--faces (elfeed-entry-tags entry)))
-         (feed (elfeed-entry-feed entry))
-         (feed-title
-          (when feed
-            (or (elfeed-meta feed :title) (elfeed-feed-title feed))))
-         (tags (mapcar #'symbol-name (elfeed-entry-tags entry)))
-         (tags-str (mapconcat
-                    (lambda (s) (propertize s 'face 'elfeed-search-tag-face))
-                    tags ","))
-         (title-width (- (window-width) 10 elfeed-search-trailing-width))
-         (title-column (elfeed-format-column
-                        title (elfeed-clamp
-                               elfeed-search-title-min-width
-                               title-width
-                               elfeed-search-title-max-width)
-                        :left))
-	 (align-to (* (+ date-width 2 (min title-width elfeed-search-title-max-width))
-		      (default-font-width))))
-    (insert (propertize date 'face 'elfeed-search-date-face) " ")
-    (insert (propertize title-column 'face title-faces 'kbd-help title) " ")
-    (valign--put-overlay (1- (point)) (point) 'display (valign--space align-to))
-    (when feed-title
-      (insert (propertize feed-title 'face 'elfeed-search-feed-face) " "))
-    (when tags
-      (insert "(" tags-str ")"))))
+  (defun elfeed-search-print-valigned-entry (entry)
+    "Print valign-ed ENTRY to the buffer."
+    (let* ((date (elfeed-search-format-date (elfeed-entry-date entry)))
+	   (date-width (car (cdr elfeed-search-date-format)))
+           (title (or (elfeed-meta entry :title) (elfeed-entry-title entry) ""))
+           (title-faces (elfeed-search--faces (elfeed-entry-tags entry)))
+           (feed (elfeed-entry-feed entry))
+           (feed-title
+            (when feed
+              (or (elfeed-meta feed :title) (elfeed-feed-title feed))))
+           (tags (mapcar #'symbol-name (elfeed-entry-tags entry)))
+           (tags-str (mapconcat
+                      (lambda (s) (propertize s 'face 'elfeed-search-tag-face))
+                      tags ","))
+           (title-width (- (window-width) 10 elfeed-search-trailing-width))
+           (title-column (elfeed-format-column
+                          title (elfeed-clamp
+				 elfeed-search-title-min-width
+				 title-width
+				 elfeed-search-title-max-width)
+                          :left))
+	   (align-to (* (+ date-width 2 (min title-width elfeed-search-title-max-width))
+			(default-font-width))))
+      (insert (propertize date 'face 'elfeed-search-date-face) " ")
+      (insert (propertize title-column 'face title-faces 'kbd-help title) " ")
+      (valign--put-overlay (1- (point)) (point) 'display (valign--space align-to))
+      (when feed-title
+	(insert (propertize feed-title 'face 'elfeed-search-feed-face) " "))
+      (when tags
+	(insert "(" tags-str ")"))))
 
-  (setq elfeed-search-print-entry-function #'elfeed-search-print-entry)
+  (setq elfeed-search-print-entry-function #'elfeed-search-print-valigned-entry)
   )
 
 (use-package rime
@@ -155,7 +155,7 @@
 
 (use-package calibredb
   :config
-  (setq calibredb-root-dir "~/Documents/calibre/")
+  (setq calibredb-root-dir "~/Sync/calibre/")
   (setq calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir))
   ;; let the seearching and jumping tool know the current directory
   (advice-add #'calibredb :after
